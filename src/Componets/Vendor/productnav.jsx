234daf7449a90693513ca255da661a3b4c0601d3
Hanumansai72 <hanumansai72@gmail.com>
2025-08-28 18:42:23 +0530

    produc nav update

--- src/Componets/Vendor/productnav.jsx
+++ src/Componets/Vendor/productnav.jsx
@@ -17,27 +17,52 @@ function ProductNavbar() {
       {/* Desktop Navbar */}
       <nav className="navbar navbar-expand-lg bg-light shadow-sm d-none d-lg-flex">
         <div className="container-fluid">
-          <Link to={`/product/${productId}`} className="navbar-brand fw-bold">Apna Mestri</Link>
+          <Link to={`/product/${productId}`} className="navbar-brand fw-bold">
+            Apna Mestri
+          </Link>
 
           <ul className="navbar-nav mx-auto mb-2 mb-lg-0 d-flex flex-row gap-3">
-            <li className="nav-item"><Link to={`/product/${productId}`} className="nav-link">Home</Link></li>
-            <li className="nav-item"><Link to={`/product/${productId}/order`} className="nav-link">Orders</Link></li>
-            <li className="nav-item"><Link to={`/product/${productId}/order/history`} className="nav-link">Order History</Link></li>
+            <li className="nav-item">
+              <Link to={`/product/${productId}`} className="nav-link">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link to={`/product/${productId}/order`} className="nav-link">Orders</Link>
+            </li>
+            <li className="nav-item">
+              <Link to={`/product/${productId}/order/history`} className="nav-link">Order History</Link>
+            </li>
           </ul>
 
           <div className="d-flex align-items-center">
             <div className="form-check form-switch me-3">
-              <input type="checkbox" className="form-check-input" checked={isOnline} onChange={() => setIsOnline(!isOnline)} />
-              <label className="form-check-label">{isOnline ? "Online" : "Offline"}</label>
+              <input
+                type="checkbox"
+                className="form-check-input"
+                checked={isOnline}
+                onChange={() => setIsOnline(!isOnline)}
+              />
+              <label className="form-check-label">
+                {isOnline ? "Online" : "Offline"}
+              </label>
             </div>
 
             <div className="dropdown">
-              <button className="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown">
+              <button
+                className="btn btn-outline-secondary dropdown-toggle"
+                type="button"
+                data-bs-toggle="dropdown"
+              >
                 Profile
               </button>
               <ul className="dropdown-menu dropdown-menu-end">
-                <li><Link to={`/product/${productId}/settings`} className="dropdown-item">My Profile</Link></li>
-                <li className="dropdown-item" onClick={signout}>Sign Out</li>
+                <li>
+                  <Link to={`/product/${productId}/settings`} className="dropdown-item">
+                    My Profile
+                  </Link>
+                </li>
+                <li className="dropdown-item" onClick={signout}>
+                  Sign Out
+                </li>
               </ul>
             </div>
           </div>
@@ -49,10 +74,10 @@ function ProductNavbar() {
         <Link to={`/product/${productId}`} className="text-center text-decoration-none text-dark">
           <i className="bi bi-house fs-5"></i><br />Home
         </Link>
-        <Link to={`/product/${productId}/orders`} className="text-center text-decoration-none text-dark">
+        <Link to={`/product/${productId}/order`} className="text-center text-decoration-none text-dark">
           <i className="bi bi-briefcase fs-5"></i><br />Orders
         </Link>
-        <Link to={`/product/${productId}/history`} className="text-center text-decoration-none text-dark">
+        <Link to={`/product/${productId}/order/history`} className="text-center text-decoration-none text-dark">
           <i className="bi bi-clock-history fs-5"></i><br />History
         </Link>
         <Link to={`/product/${productId}/settings`} className="text-center text-decoration-none text-dark">
